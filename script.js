4bb13c62c47e1792de7e3610848477b86c1188a8
WasimAkhtar1 <wasimlakhtar@gmail.com>
2023-11-03 15:06:00 +0530

    Project Completed

---
+++ script.js
@@ -0,0 +1,246 @@
+var x=gsap.timeline()
+
+.from("#nav,#nav img,#nav ul a,.icon i",{
+    opacity:0,
+    stagger:0.1,
+    duration:.5,
+    y:-50,
+
+})
+
+.from(".part",{
+    stagger:0.3,
+    duration:.5,
+    x:-100,
+    opacity:0,
+
+})
+
+.from(".icon2 ",{
+    opacity:0,
+    duration:.5,
+    y:-50,
+    stagger:0.4,
+
+})
+
+.from(".part2 ",{
+  opacity:0,
+  duration:.6,
+  y:50,
+  stagger:0.5
+})
+
+.from(".part3,.part4",{
+    opacity:0,
+    duration:.7,
+    x:50,
+})
+
+
+
+.from(".center",{
+    opacity:0,
+    duration:.7,
+    x:-100,
+    delay:1,
+    stagger:0.5,
+})
+
+.from(".concept h4,.concept h2,.concept p,.icon4 h5,.icon4 i",{
+    opacity:0,
+    duration:.7,
+    x:-50,
+    stagger:0.3,
+})
+
+.from(".hello,.fuck",{
+    stagger:0.3,
+    duration:.8,
+    x:50,
+    opacity:0,
+})
+
+.from("#btn1",{
+    stagger:0.3,
+    opacity:0,
+    x:-50,
+    duration:.8
+})
+
+.from(".nav-part2",{
+    opacity:0,
+    duration:.8,
+    stagger:0.3,
+    x:-50,
+})
+
+.from(".main",{
+    duration:.8,
+    stagger:0.4,
+    x:-60,
+    opacity:0,
+})
+
+.from(".console",{
+    opacity:0,
+    duration:.7,
+    stagger:0.3,
+    x:50
+})
+
+.from(".main-point",{
+    stagger:0.4,
+    duration:0.4,
+    Y:-50,
+    opacity:0
+})
+
+.from(".vector",{
+    duration:0.4,
+    stagger:0.4,
+    x:-50,
+    opacity:0,
+})
+
+.from(".contact",{
+    stagger:0.3,
+    duration:0.8,
+    y:50,
+    opacity:0,
+})
+
+.from(".rate",{
+    opacity:0,
+    duration:0.8,
+    x:50,
+    stagger:0.5,
+})
+
+.from(".king",{
+    opacity:0,
+    duration:0.7,
+    x:-50,
+    stagger:0.3
+})
+
+.from(".heat",{
+    opacity:0,
+    duration:0.8,
+    stagger:0.5,
+    y:50,
+})
+
+.from(".vertical,.head,.traker,.text,.jhat",{
+    opacity:0,
+    duration:0.8,
+    stagger:0.4,
+    x:50,
+})
+
+.from(".nav-part6",{
+    opacity:0,
+    duration:0.8,
+    x:-50,
+    stagger:0.4,
+})
+
+.from(".talent",{
+    stagger:0.8,
+    opacity:0,
+    x:50,
+    duration:0.6,
+})
+
+.from(".slider",{
+    duration:0.6,
+    stagger:0.7,
+    opacity:0,
+    y:-100,
+})
+
+.from(".nav3",{
+    duration:0.8,
+    opacity:0,
+    x:-100,
+    stagger:.3,
+})
+
+.from(".our",{
+    opacity:0,
+    duration:0.9,
+    x:-50,
+    stagger:.3,
+})
+
+.from(".our2",{
+    opacity:0,
+    duration:0.7,
+    x:50,
+    stagger:0.4,
+})
+
+.from(".dry",{
+    opacity:0,
+    duration:0.7,
+    y:60,
+    stagger:0.5,
+})
+
+.from(".pro",{
+    opacity:0,
+    duration:0.8,
+    y:-50,
+    stagger:0.4,
+})
+
+.from(".pro1",{
+    opacity:0,
+    duration:1,
+    x:-50,
+    stagger:0.4,
+})
+
+.from(".main-line",{
+    opacity:0,
+    duration:0.8,
+    x:50,
+    stagger:0.4,
+})
+
+.from(".screen",{
+    opacity:0,
+    duration:0.7,
+    x:-50,
+    stagger:0.3,
+})
+
+.from(".scare",{
+    opacity:0,
+    duration:0.8,
+    stagger:0.4,
+    x:50,
+})
+
+.from(".bolo",{
+    opacity:0,
+    stagger:0.3,
+    y:-50,
+    duration:1,
+})
+
+.from(".script",{
+    opacity:0,
+    duration:0.7,
+    x:50,
+    stagger:0.3,
+    
+})
+
+.from(".border,.condom",{
+    opacity:0,
+    duration:0.8,
+    y:-100,
+    stagger:0.4,
+
+})
